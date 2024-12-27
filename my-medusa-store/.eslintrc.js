8d552ab86dc514f14f865c831b5af64983660c6b
raniarkal <raniarkal22@gmail.com>
2024-12-27 12:45:16 +0530

    commit by rani

---
+++ my-medusa-store/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals'
+  ],
+  rules: {
+    // Customize rules as needed
+    '@next/next/no-html-link-for-pages': 'off'
+  }
+}
---
+++ my-medusa-store/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Your Medusa Storefront</h1>
+    </div>
+  )
+}
